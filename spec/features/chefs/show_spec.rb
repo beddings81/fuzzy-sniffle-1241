b62f815dd6a2eee1dc3d7db16dbe7a64c1ef3d72
Braxton Eddings <beddings81@gmail.com>
2023-01-08 23:28:50 -0600

    Finish us 3

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -33,7 +33,7 @@ RSpec.describe 'chefs show page' do
     end
 
     expect(page).to have_field "dish_id"
-    
+
     fill_in('dish_id', with: "#{dish3.id}")
 
     click_button 'Submit'
@@ -43,5 +43,30 @@ RSpec.describe 'chefs show page' do
     within("#dishes") do
       expect(page).to have_content("Loaded Baked Potato")
     end
+  end
+
+  it 'links to a chefs ingredients index page' do
+    chef = Chef.create!(name: "Bron")
+    dish = Dish.create!(name: "Seafood Pasta", description: 'Served with rolls', chef_id: chef.id)
+    dish2 = Dish.create!(name: "Grilled Chicken", description: 'Served with bbq sauce', chef_id: chef.id)
+    dish3 = Dish.create!(name: "Loaded Baked Potato", description: 'Served with butter', chef_id: chef.id)
+    ingredient1 = dish.ingredients.create!(name: "Shrimp", calories: 500)
+    ingredient2 = dish.ingredients.create!(name: "Pasta", calories: 750)
+    ingredient3 = dish2.ingredients.create!(name: "Chicken", calories: 500)
+    ingredient4 = dish3.ingredients.create!(name: "Potato", calories: 750)
+
+    visit chef_path(chef)
+
+    expect(page).to have_link("#{chef.name}'s Ingredients")
+
+    click_link "#{chef.name}'s Ingredients"
+
+    expect(current_path).to eq(chef_ingredients_path(chef))
+
+    expect(page).to have_content("Chef Bron's Ingredients")
+    expect(page).to have_content("Shrimp")
+    expect(page).to have_content("Pasta")
+    expect(page).to have_content("Chicken")
+    expect(page).to have_content("Potato")
   end
 end
